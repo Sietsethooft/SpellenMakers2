c8d496480d0efe0cd6f57f6d6541e17f05ef1f43
Sietsethooft <147145688+Sietsethooft@users.noreply.github.com>
2024-10-15 18:06:59 +0200

    Renaming GenderEnum class
    Renamed GenderEnum --> Gender

---
+++ SpellenMakers/Core.Domain/Gender.cs
@@ -0,0 +1,9 @@
+﻿namespace Core.Domain;
+
+public class Gender
+{
+    public int GenderId { get; set; }
+    public required string Name { get; set; }
+
+    public ICollection<User>? Users { get; set; }
+}
