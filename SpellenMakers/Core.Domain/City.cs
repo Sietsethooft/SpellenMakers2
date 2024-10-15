fac10d58a651927a1a17c853648756921d1f125e
Sietsethooft <147145688+Sietsethooft@users.noreply.github.com>
2024-10-15 15:08:51 +0200

    Creation City Table

---
+++ SpellenMakers/Core.Domain/City.cs
@@ -0,0 +1,9 @@
+﻿namespace Core.Domain;
+
+public class City
+{
+    public int CityId { get; set; }
+    public required string Name { get; set; }
+
+    public ICollection<Address>? Addresses { get; set; }
+}
