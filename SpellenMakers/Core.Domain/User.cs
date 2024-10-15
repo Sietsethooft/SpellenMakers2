69f3cc0bc1271a0580cc9003d80568b895d2e53d
Sietsethooft <147145688+Sietsethooft@users.noreply.github.com>
2024-10-15 15:08:04 +0200

    Creation User table

---
+++ SpellenMakers/Core.Domain/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Domain;
+
+public class User
+{
+    public int UserId { get; set; }
+    public required string Name { get; set; }
+    public required string Email { get; set; }
+    public DateTime Birthday { get; set; }
+
+    public GenderEnum GenderId { get; set; } // Foreign Key to GenderEnum
+    public required Dietary DietaryId { get; set; } // Foreign Key to Dietary
+    public required Address AddressId { get; set; } // Foreign Key to Address
+
+    public ICollection<GameNight>? OrganizedGameNights { get; set; }
+    public ICollection<Player>? PlaysIn { get; set; }
+}
