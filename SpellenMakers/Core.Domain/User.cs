996a408da0c3782d26e63a0c77d7af0cca38014f
Sietsethooft <147145688+Sietsethooft@users.noreply.github.com>
2024-10-15 18:05:29 +0200

    Update User.cs
    - Removed unnecessary imports
    
    + Added navigation properties
    +/- Changed foreign key names.

--- SpellenMakers/Core.Domain/User.cs
+++ SpellenMakers/Core.Domain/User.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net.Sockets;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Core.Domain;
+﻿namespace Core.Domain;
 
 public class User
 {
@@ -14,9 +7,14 @@ public class User
     public required string Email { get; set; }
     public DateTime Birthday { get; set; }
 
-    public GenderEnum GenderId { get; set; } // Foreign Key to GenderEnum
-    public required Dietary DietaryId { get; set; } // Foreign Key to Dietary
-    public required Address AddressId { get; set; } // Foreign Key to Address
+    public int GenderId { get; set; } // Navigation property
+    public required Gender Gender { get; set; } // Foreign Key to GenderEnum
+
+    public int DietaryId { get; set; } // Navigation property
+    public required Dietary Dietary { get; set; } // Foreign Key to Dietary
+
+    public int AddressId { get; set; } // Navigation property
+    public required Address Address { get; set; } // Foreign Key to Address
 
     public ICollection<GameNight>? OrganizedGameNights { get; set; }
     public ICollection<Player>? PlaysIn { get; set; }
