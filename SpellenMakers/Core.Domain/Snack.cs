2e488ccd8b8a6b3fb6ea71d5610d111e494db180
Sietsethooft <147145688+Sietsethooft@users.noreply.github.com>
2024-10-15 15:08:13 +0200

    Creation snack table

---
+++ SpellenMakers/Core.Domain/Snack.cs
@@ -0,0 +1,13 @@
+﻿namespace Core.Domain;
+
+public class Snack
+{
+    public int SnackId { get; set; }
+    public required string Name { get; set; }
+    public bool IsVegetarian { get; set; }
+    public bool HasAlcohol { get; set; }
+    public bool HasLactose { get; set; }
+    public bool HasNuts { get; set; }
+
+    public ICollection<GameNight_Snack>? GameNight_Snacks { get; set; }
+}
