ae4198d9889396a7dfd5116187476dae9a3439af
Sietsethooft <147145688+Sietsethooft@users.noreply.github.com>
2024-10-15 15:08:37 +0200

    Creation GameNight table

---
+++ SpellenMakers/Core.Domain/GameNight.cs
@@ -0,0 +1,17 @@
+﻿namespace Core.Domain;
+
+public class GameNight
+{
+    public int GameNightId { get; set; }
+    public int MaxCountPlayers { get; set; }
+    public DateTime Date { get; set; }
+    public TimeOnly Time { get; set; }
+    public bool BringSnacks { get; set; }
+
+    public required User Organizer { get; set; } // Foreign Key to User
+    public required Address AddressId { get; set; } // Foreign Key to Address
+
+    public ICollection<Player>? Players { get; set; }
+    public ICollection<GameNight_Snack>? Snacks { get; set; }
+    public required ICollection<GameNight_Games> Games { get; set; }
+}
