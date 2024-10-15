a93e6c60dfb0ac56a2bef1678de8cc5a7c15d538
Sietsethooft <147145688+Sietsethooft@users.noreply.github.com>
2024-10-15 15:09:47 +0200

    Creation GameNight_Games table
    This table is the connection between the many to many relationship of GameNight and Game.

---
+++ SpellenMakers/Core.Domain/GameNight_Games.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Core.Domain;
+
+public class GameNight_Games
+{
+    [Key]
+    public int GameName { get; set; }
+    public int GameNightId { get; set; }
+
+    public required Game Game { get; set; } // Foreign Key to Game
+    public required GameNight GameNight { get; set; } // Foreign Key to GameNight
+}
