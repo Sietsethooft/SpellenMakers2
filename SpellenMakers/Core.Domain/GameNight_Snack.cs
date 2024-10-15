2e9a69aa2fcd477fc0db3277fcf965313adbeb34
Sietsethooft <147145688+Sietsethooft@users.noreply.github.com>
2024-10-15 15:10:05 +0200

    Creation GameNight_Snack table
    This table is the connection between the many to many relationship of GameNight and Snack.

---
+++ SpellenMakers/Core.Domain/GameNight_Snack.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Domain;
+
+public class GameNight_Snack
+{
+    public int GameNightId { get; set; }
+    public int SnackId { get; set; }
+
+    public required GameNight GameNight { get; set; } // foreign key to GameNight
+    public required Snack Snack { get; set; } // foreign key to Snack
+}
