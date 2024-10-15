6c5971d4737d8b5fb17a1398ff509c857687d624
Sietsethooft <147145688+Sietsethooft@users.noreply.github.com>
2024-10-15 15:10:26 +0200

    Creation Player table
    This table is the connection between the many to many relationship of GameNight and User.

---
+++ SpellenMakers/Core.Domain/Player.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Domain;
+
+public class Player
+{
+    public int UserId { get; set; }
+    public int GameNightId { get; set; }
+    
+    public required User User { get; set; } // foreign key to User
+    public required GameNight GameNight { get; set; } // foreign key to GameNight
+}
