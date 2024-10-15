5e346cca5f29d328d937102eb7959a601c546ec9
Sietsethooft <147145688+Sietsethooft@users.noreply.github.com>
2024-10-15 15:09:08 +0200

    Creation Address Table

---
+++ SpellenMakers/Core.Domain/Address.cs
@@ -0,0 +1,10 @@
+﻿namespace Core.Domain;
+
+public class Address
+{
+    public int AddressId { get; set; }
+    public required string HouseNumber { get; set; }
+    public required string Street { get; set; }
+    public required City CityId { get; set; } // Foreign Key to City
+    public ICollection<User>? Users { get; set; }
+}
