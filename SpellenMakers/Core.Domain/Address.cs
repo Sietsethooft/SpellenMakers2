644fe5bbb7e2bbca424eb335df5eadfda0039a52
Sietsethooft <147145688+Sietsethooft@users.noreply.github.com>
2024-10-15 18:09:32 +0200

    Update Address.cs
    + Added navigation properties
    + Added GameNights collection
    +/- Changed foreign key names.

--- SpellenMakers/Core.Domain/Address.cs
+++ SpellenMakers/Core.Domain/Address.cs
@@ -5,6 +5,10 @@ public class Address
     public int AddressId { get; set; }
     public required string HouseNumber { get; set; }
     public required string Street { get; set; }
-    public required City CityId { get; set; } // Foreign Key to City
+
+    public int CityId { get; set; } // Navigation property
+    public required City City { get; set; } // Foreign Key to City
+
     public ICollection<User>? Users { get; set; }
+    public ICollection<GameNight>? GameNights { get; set; }
 }
