3499caf5f17542b7b6da7562dd7f423a8e3b7a33
Sietsethooft <147145688+Sietsethooft@users.noreply.github.com>
2024-10-15 15:09:00 +0200

    Creation CategoryEnum table

---
+++ SpellenMakers/Core.Domain/CategoryEnum.cs
@@ -0,0 +1,8 @@
+﻿namespace Core.Domain;
+
+public class CategoryEnum
+{
+    public int CategoryId { get; set; }
+    public required string Category { get; set; }
+    public ICollection<Game>? Games { get; set; }
+}
