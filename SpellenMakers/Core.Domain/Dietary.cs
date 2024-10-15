02967d7490b5af2dae31695575a837765a91eb61
Sietsethooft <147145688+Sietsethooft@users.noreply.github.com>
2024-10-15 15:09:15 +0200

    Creation Dietary table

---
+++ SpellenMakers/Core.Domain/Dietary.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Domain;
+
+public class Dietary
+{
+    public int DietaryId { get; set; }
+    public bool isVegetarian { get; set; }
+    public bool WantsAlcohol { get; set; }
+    public bool HasLactose { get; set; }
+    public bool HasNutAllergy { get; set; }
+}
