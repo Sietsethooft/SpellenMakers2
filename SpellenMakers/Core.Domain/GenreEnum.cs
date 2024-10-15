c2dd35eac9d362821c37e499951b99a050525065
Sietsethooft <147145688+Sietsethooft@users.noreply.github.com>
2024-10-15 15:08:21 +0200

    Creation GenreEnum table

---
+++ SpellenMakers/Core.Domain/GenreEnum.cs
@@ -0,0 +1,8 @@
+﻿namespace Core.Domain;
+
+public class GenreEnum
+{
+    public int GenreId { get; set; }
+    public required string Genre { get; set; }
+    public ICollection<Game>? Games { get; set; }
+}
