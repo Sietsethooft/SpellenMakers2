3b9f3eebeba753f78c11d89edaf0b29799eeee7e
Sietsethooft <147145688+Sietsethooft@users.noreply.github.com>
2024-10-15 18:12:14 +0200

    Adding foreign keys
    Added all foreign keys

--- SpellenMakers/Infrastructure/SpellenMakersContext.cs
+++ SpellenMakers/Infrastructure/SpellenMakersContext.cs
@@ -20,6 +20,104 @@ public class SpellenMakersContext : DbContext
     public DbSet<GameNight_Games> GameNight_Games { get; set; }
     public DbSet<Snack> Snacks { get; set; }
 
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        // Configuratie voor GameNight_Games
+        modelBuilder.Entity<GameNight_Games>()
+            .HasKey(gn => new { gn.GameNightId, gn.GameId });
+
+        // Configuratie voor GameNight_Snack
+        modelBuilder.Entity<GameNight_Snack>()
+            .HasKey(gs => new { gs.GameNightId, gs.SnackId });
+
+        // Configuratie voor Player
+        modelBuilder.Entity<Player>()
+            .HasKey(p => new { p.UserId, p.GameNightId });
+
+        // Configuratie voor foreign relaties
+
+
+        modelBuilder.Entity<User>()
+            .HasOne(u => u.Gender)
+            .WithMany(g => g.Users)
+            .HasForeignKey(u => u.GenderId)
+            .OnDelete(DeleteBehavior.NoAction);
+
+        modelBuilder.Entity<User>()
+            .HasOne(u => u.Dietary)
+            .WithMany()
+            .HasForeignKey(u => u.DietaryId)
+            .OnDelete(DeleteBehavior.NoAction);
+
+        modelBuilder.Entity<User>()
+            .HasOne(u => u.Address)
+            .WithMany(a => a.Users)
+            .HasForeignKey(u => u.AddressId)
+            .OnDelete(DeleteBehavior.NoAction);
+
+        modelBuilder.Entity<Address>()
+            .HasOne(a => a.City)
+            .WithMany(c => c.Addresses)
+            .HasForeignKey(a => a.CityId)
+            .OnDelete(DeleteBehavior.NoAction);
+
+        modelBuilder.Entity<Player>()
+            .HasOne(p => p.User)
+            .WithMany(u => u.PlaysIn)
+            .HasForeignKey(p => p.UserId);
+
+        modelBuilder.Entity<Player>()
+            .HasOne(p => p.GameNight)
+            .WithMany(gn => gn.Players)
+            .HasForeignKey(p => p.GameNightId)
+            .OnDelete(DeleteBehavior.NoAction);
+
+        modelBuilder.Entity<GameNight>()
+            .HasOne(gn => gn.Organizer)
+            .WithMany(u => u.OrganizedGameNights)
+            .HasForeignKey(gn => gn.OrganizerId);
+
+        modelBuilder.Entity<GameNight>()
+            .HasOne(gn => gn.Address)
+            .WithMany(a => a.GameNights)
+            .HasForeignKey(gn => gn.AddressId)
+            .OnDelete(DeleteBehavior.NoAction);
+
+        modelBuilder.Entity<GameNight_Snack>()
+            .HasOne(gs => gs.GameNight)
+            .WithMany(gn => gn.Snacks)
+            .HasForeignKey(gs => gs.GameNightId);
+
+        modelBuilder.Entity<GameNight_Snack>()
+            .HasOne(gs => gs.Snack)
+            .WithMany(s => s.GameNight_Snacks)
+            .HasForeignKey(gs => gs.SnackId);
+
+        modelBuilder.Entity<GameNight_Games>()
+            .HasOne(gg => gg.GameNight)
+            .WithMany(gn => gn.Games)
+            .HasForeignKey(gg => gg.GameNightId);
+
+        modelBuilder.Entity<GameNight_Games>()
+            .HasOne(gg => gg.Game)
+            .WithMany(g => g.GameNight_Games)
+            .HasForeignKey(gg => gg.GameId);
+
+        modelBuilder.Entity<Game>()
+            .HasOne(g => g.Genre)
+            .WithMany(ge => ge.Games)
+            .HasForeignKey(g => g.GenreId)
+            .OnDelete(DeleteBehavior.NoAction);
+
+        modelBuilder.Entity<Game>()
+            .HasOne(g => g.Category)
+            .WithMany(ce => ce.Games)
+            .HasForeignKey(g => g.CategoryId)
+            .OnDelete(DeleteBehavior.NoAction);
+
+        base.OnModelCreating(modelBuilder);
+    }
+
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
         optionsBuilder.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=spellenmakers;Trusted_Connection=True;");
